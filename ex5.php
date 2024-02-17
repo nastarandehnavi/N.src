11703df00ee582c4c20136dac0ef6e2fbf7db8b7
nastarandehnavi <nastaran23000@student.hamk.fi>
2024-02-17 11:32:45 +0200

    task5

---
+++ ex5.php
@@ -0,0 +1,23 @@
+<?php
+$title = "Exercise 5";
+
+
+include 'header.php' ?>
+
+        <h2>Input your information</h2>
+        <h2>4.2:  Check Eligibility for Voting :</h2>
+    <form method="post" action="README.md">
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name" required><br><br>
+
+    </form>
+            
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+    <!-- Bootstrap JS and jQuery -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    
+    <?php include 'footer.php' ?>
