b241bec8fe32cca5bca4b398ab4b6f6850100ebf
nastarandehnavi <nastaran23000@student.hamk.fi>
2024-02-17 12:16:10 +0200

    ex5

--- ex5.php
+++ ex5.php
@@ -5,8 +5,13 @@
    include 'header.php' ?>
  
 
- < class="nav-item">
+ <li class="nav-item">
           <a class="nav-link" href="https://github.com/POORY-AZARE/php.teamwork/blob/main/README.md">README.md</a>
-       
+        </li>
 
+
+
+        <a href="https://github.com/POORY-AZARE/php.teamwork/blob/main/README.md"target="blank"
+        >README</a
+      >
 <?php include 'footer.php' ?>
