cf393646c596ef8604cea2a69d523c2452d9d6d1
nastarandehnavi <nastaran23000@student.hamk.fi>
2024-02-17 12:32:02 +0200

    ex5

--- ex5.php
+++ ex5.php
@@ -4,11 +4,7 @@
 ?>
 
 <style>
-  /* Add your styles here */
-  .nav-item {
-    color:blue
-    /* Your styles for nav-item */
-  }
+ 
   
   .nav-link {
     color: black; /* Set the color of the link to black */
@@ -16,8 +12,6 @@
   }
 </style>
 
- <li>
-    <a class="nav-link" href="https://github.com/POORY-AZARE/php.teamwork/blob/main/README.md">README.md</a> 
- </li>
+ <li><a class="nav-link" href="https://github.com/POORY-AZARE/php.teamwork/blob/main/README.md">README.md</a> </li>
 
 <?php include 'footer.php'; ?>
